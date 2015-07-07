d91dbaec1fa4efad6dc7dfe16be9cf189ca4433b
Kaunteya Suryawanshi <k.suryawanshi@gmail.com>
2015-07-07 21:23:18 +0530

    Initial Commit

---
+++ FlashLabel/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  FlashLabel
+//
+//  Created by Kauntey Suryawanshi on 07/07/15.
+//  Copyright (c) 2015 Kauntey Suryawanshi. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
