70ccb4f1f75c8b24fe1b2556b8ffc7a6556cce36
Kaunteya Suryawanshi <k.suryawanshi@gmail.com>
2015-07-09 12:10:39 +0530

    Test changes

--- FlashLabel.swift
+++ FlashLabel.swift
@@ -18,25 +18,75 @@ public class FlashLabel: NSTextField {
     
     required public init?(coder: NSCoder) {
         super.init(coder: coder)
+        self.wantsLayer = true
+//        self.layer!.contentsGravity = kCAGravityCenter
+        self.layer!.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+
+        println("Anchor point \(self.layer!.anchorPoint)")
         self.setVisibility(false, animated: false)
+        makeShowAnimation()
+        makeHideAnimation()
     }
-    
+
     var visible: Bool {
         get {
-            return self.alphaValue == 1
+            return self.layer!.opacity == 1
         }
     }
+
+    var showAnimation: CABasicAnimation!
+    func makeShowAnimation() {
+        showAnimation = CABasicAnimation(keyPath: "opacity")
+        showAnimation.duration = 1
+        showAnimation.fromValue = 0
+        showAnimation.toValue = 1
+        showAnimation.repeatCount = 0
+    }
+
+    var hideAnimation: CAAnimationGroup!
+    func makeHideAnimation() {
+        var scaleXAnimation = CABasicAnimation(keyPath: "transform.scale.x")
+        scaleXAnimation.fromValue = 1
+        scaleXAnimation.toValue = 1.2
+        scaleXAnimation.duration = 1
+        scaleXAnimation.fillMode = kCAFillModeForwards
+        
+        
+        var boundsAnimation = CABasicAnimation(keyPath: "bounds")
+        boundsAnimation.fromValue =  NSValue(rect: self.bounds)
+        var newBounds = self.bounds
+        newBounds.size.width += 50
+        newBounds.size.height += 20
+        boundsAnimation.toValue = NSValue(rect: newBounds)
+        
+        
+        var scaleYAnimation = CABasicAnimation(keyPath: "transform.scale.y")
+        scaleYAnimation.fromValue = 1
+        scaleYAnimation.toValue = 1.5
+        scaleYAnimation.duration = 1
+        scaleYAnimation.fillMode = kCAFillModeForwards
+
+        var opacityAnimation = CABasicAnimation(keyPath: "opacity")
+        opacityAnimation.fromValue = 1
+        opacityAnimation.toValue = 0
+        opacityAnimation.repeatCount = 0
+
+        hideAnimation = CAAnimationGroup()
+        hideAnimation.animations = [scaleXAnimation, boundsAnimation]
+//        hideAnimation.animations = [scaleXAnimation, opacityAnimation]
+        hideAnimation.duration = 1
+        hideAnimation.delegate = self
+        }
     
     func setVisibility(enabled: Bool, animated: Bool) {
         if animated {
-            NSAnimationContext.beginGrouping()
-            NSAnimationContext.currentContext().duration = 1.5
-            NSAnimationContext.currentContext().timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseIn)
-            self.animator().alphaValue = enabled ? 1 : 0
-            NSAnimationContext.endGrouping()
-        } else {
-            self.alphaValue = enabled ? 1 : 0
+            if enabled {
+                self.layer!.addAnimation(showAnimation, forKey: "visible")
+            } else {
+                self.layer!.addAnimation(hideAnimation, forKey: "hidden")
+            }
         }
+//        self.layer!.opacity  = enabled ? 1 : 0
     }
     
     /**
@@ -51,7 +101,6 @@ public class FlashLabel: NSTextField {
         self.setVisibility(true, animated: false)
         self.stringValue = text
         self.sizeToFit()
-        
         if flash {
             timeSummation = 0
             showTime = time
@@ -73,5 +122,8 @@ public class FlashLabel: NSTextField {
             timer.invalidate()
             self.setVisibility(false, animated: false)
         }
+    }
+    public override func animationDidStop(anim: CAAnimation!, finished flag: Bool) {
+        println("Animation stopped")
     }
 }
